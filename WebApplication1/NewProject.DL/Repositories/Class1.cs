8a62c075e12a1dbd9d0ff1b3b9c59d0e526e5b15
Vasil Peychinov <vaskobe1997@gmail.com>
2024-11-04 19:19:13 +0200

    Layering

---
+++ WebApplication1/NewProject.DL/Repositories/Class1.cs
@@ -0,0 +1,22 @@
+﻿using NewProject.DL.Interfaces;
+using NewProject.DL.StaticDB;
+using NewProject.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject.DL.Repositories
+{
+    internal class MovieRepository : IMovieRepository
+    {
+        public List<Movie> GetAllMovies
+        {
+            return MemoryDb.movies;
+            {
+
+            }
+        }
+    }
+}
