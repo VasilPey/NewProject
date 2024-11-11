7fa9f00e1f5011d12bc32ff9d0c88ba5c66b3bce
VasilPey <vaskobe1997@gmail.com>
2024-11-11 18:27:59 +0200

    AddMovie

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -7,18 +7,25 @@ namespace WebApplication1
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services
-                .AddSingleton<IMovieService, MovieService>;
             builder.Services.AddControllers();
 
-            var app = builder.Build();
 
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            var app = builder.Build();
+ 
             // Configure the HTTP request pipeline.
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.MapControllers();
 
